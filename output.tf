438eeca9621c3a0bab91a8d84292c3ebe997a63a
Christian Nuss <christian.nuss@gmail.com>
2021-07-13 17:26:13 -0700

    Output the topic ARNs

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
 output "bucket_name" {
-  value = aws_s3_bucket.bucket.id
+  value       = aws_s3_bucket.bucket.id
+  description = "The bucket name with a randomized suffix"
+}
+
+output "topic_arns" {
+  value       = aws_sns_topic.topic[*].arn
+  description = "The topic ARNs created (if any)"
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,34 @@
-variable "bucket_name_prefix" {}
+variable "bucket_name_prefix" {
+  type        = string
+  description = "A prefix name for the bucket"
+}
 
 variable "read_only_principals" {
-  default = []
+  default     = []
+  description = "The ARNs of users/roles/etc that have read-only access to the bucket"
 }
 
 variable "read_write_principals" {
-  default = []
+  default     = []
+  description = "The ARNs of users/roles/etc that have read-write access to the bucket"
 }
 
 variable "write_only_principals" {
-  default = []
+  default     = []
+  description = "The ARNs of users/roles/etc that have write-only access to the bucket"
 }
 
 variable "write_only_services" {
-  default = []
+  default     = []
+  description = "The AWS services that have write-only access to the bucket (e.g. ses.amazonaws.com, lambda.amazonaws.com)"
 }
 
 variable "root_principal" {
-  default = "root"
-}
-
-variable "allowed_origins" {
-  default = ["*"]
+  default     = "root"
+  description = "The root prinicipal. In most cases leave this as 'root'"
 }
 
 variable "notification_prefixes" {
-  default = []
+  default     = []
+  description = "The object prefixes to create SNS notifications for (e.g. ['folder1/', 'folder2/'] or [''] for everything)"
 }
